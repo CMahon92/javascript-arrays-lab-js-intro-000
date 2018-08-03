e71cba72578a7ae0425d49e89408e0f1c33b1aca
Carleen mahon <carleenmahon92@gmail.com>
2018-08-03 23:44:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
 // // Add your functions and code here
-// 
+//
 // //Part 1
-// 
+//
 // var kittens = ['Milo', 'Otis', 'Garfield'];
-// 
+//
 // function JSKittenLab(kittens) {
-// 
+//
 //   return kittens;
-// 
+//
 // }
-// 
+//
 // function destructivelyAppendKitten(kittens) {
 //   kittens.push['Ralph'];
 //   return kittens;
-// 
+//
 // }
 
 // function destructivelyPrependKitten(name) {
-// 
+//
 // }
-// 
+//
 // function appendKitten(name) {
-// 
+//
 // }
